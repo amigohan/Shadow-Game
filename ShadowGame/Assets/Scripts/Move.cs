c51c6f3ee5b88a775923955793b1263191bd059a
amigohan <tonanpetek.tr@gmail.com>
2024-01-06 12:43:19 +0300

    code

--- ShadowGame/Assets/Scripts/Move.cs
+++ ShadowGame/Assets/Scripts/Move.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    
+    public Animator animator;
+
 
     void Update()
     {
         // Yatay ve dikey girişleri al
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+
+        animator.setFloat("Horizontal", horizontalInput);
+        animator.setFloat("Vertical", verticalInput);
+        animator.setFloat("Speed", movement.sqrMagnitude);
 
         // Hareket vektörünü oluştur
         Vector2 movement = new Vector2(horizontalInput, verticalInput);
