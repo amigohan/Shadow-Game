4a10e2037edce4f465f8d30dbc9134c04f2ef50e
Isaac7088 <isaacviberlofi@gmail.com>
2024-01-08 01:01:07 +0300

    :(

--- ShadowGame/Assets/Scripts/BridgeWood.cs
+++ ShadowGame/Assets/Scripts/BridgeWood.cs
@@ -4,18 +4,14 @@ using UnityEngine;
 
 public class BridgeWood : MonoBehaviour
 {
-    [SerializeField] GameObject Obs;
-    [SerializeField] GameObject Obs2;
-    [SerializeField] GameObject Obs3;
+    [SerializeField] GameObject Bridge;
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         // WoodColl s�n�f�nda wood de�i�keni static oldu�u i�in do�rudan s�n�f ad� �zerinden eri�ilebilir.
         if (WoodColl.wood == 3)
         {
-            Obs.gameObject.SetActive(true);
-            Obs2.gameObject.SetActive(true);
-            Obs3.gameObject.SetActive(true);
+            Bridge.gameObject.SetActive(true);
             Destroy(gameObject);
         }
     }
